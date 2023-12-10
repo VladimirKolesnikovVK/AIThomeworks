cb006867a33b836017b90ba178461dfdcdf83fb5
VladimirKolesnikovVK <147302582+VladimirKolesnikovVK@users.noreply.github.com>
2023-12-10 14:42:34 +0100

    №58 Фильтрация списка: Создайте список строк. Используя лямбда-выражения и метод filter из Stream API, отфильтруйте и выведите на экран все строки, которые начинаются на букву "А".
    Преобразование элементов списка: Имеется список целых чисел. Используя лямбда-выражение, преобразуйте каждое число в его квадрат и сохраните результаты в новом списке.
    
    Лямбда-выражение для работы с потоком данных: Используя поток данных (Stream) из списка целых чисел, напишите лямбда-выражение для фильтрации всех четных чисел и их последующего вывода на экран.
    
    Использование forEach с лямбда-выражениями: Имеется список целых чисел. Используя лямбда-выражение в методе forEach, напечатайте каждый элемент списка.
    
    Работа с потоками (Streams): Используйте потоки для обработки коллекций с помощью лямбда-выражений,преобразуйте все строки в списке в верхний регистр

---
+++ src/main/java/de/ait/homework58/LambdaToWork.java
@@ -0,0 +1,41 @@
+package de.ait.homework58;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class LambdaToWork {
+    public static void main(String[] args) {
+        //Фильтрация списка
+        List<String> lambdas = Arrays.asList("lambda","ambda","mbda","bda","da","a"," ");
+        String foundLambda = lambdas.stream().filter(lambda -> lambda.startsWith("a") && lambda.length()>0).findFirst().orElse("not found");
+        System.out.println(foundLambda);
+
+        System.out.println("-------------------------");
+
+        //Преобразование элементов списка
+        List<Integer> intNumbers = Arrays.asList(2,3,4,55);
+        intNumbers.replaceAll(element-> element*element);
+        List<Integer> squareNumbers = intNumbers;
+        System.out.println(squareNumbers);
+
+        System.out.println("-------------------------");
+
+        //Лямбда-выражение для работы с потоком данных
+        List<Integer> evenNumbers = Arrays.asList(2,3,4,6,8,1,23,22,31,32);
+        evenNumbers.stream().filter(element-> element%2==0).forEach(System.out::println);
+
+        System.out.println("-------------------------");
+
+        //Использование forEach с лямбда-выражениями
+        List<Integer> forEachNumbers = Arrays.asList(2,3,4,6,8,1,23,22,31,32);
+        forEachNumbers.forEach(number -> System.out.println(number));
+
+        System.out.println("-------------------------");
+
+        //Работа с потоками (Streams)
+        List<String> words = Arrays.asList("bmw","porsche","mercedes","audi");
+        List<String> newWords = words.stream().map(String::toUpperCase).collect(Collectors.toList());
+        newWords.forEach(System.out::println);
+    }
+}
